357370faaadd7e4fb54f1277612d33337acd8d46
Gonzo Fialho <carloseaf@gmail.com>
2018-01-12 20:50:08 -0200

    Initial Commit

---
+++ Swiftitle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swiftitle
+//
+//  Created by Gonzo Fialho on 12/01/18.
+//  Copyright © 2018 Gonzo Fialho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swiftitle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swiftitle
+//
+//  Created by Gonzo Fialho on 12/01/18.
+//  Copyright © 2018 Gonzo Fialho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
